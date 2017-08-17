12a5716a58bada65a0f612e9e4d775dc318b03c3
ezradiniz <linux.esdras@gmail.com>
2017-08-17 18:10:48 -0300

    Added network station details

--- api/controller/networkController.js
+++ api/controller/networkController.js
@@ -1,16 +1,29 @@
 const mongoose = require('mongoose');
 const Network = mongoose.model('Networks');
+const NetworkStation = mongoose.model('NetworksStations');
 
 class NetworkController {
 
 	static listAllNetworks(req, res) {
-		Network.find({})
-				.then((data) => {
-					res.json(data);
-				}).catch((err) => {
-					res.send(err);
-				});
+		Network
+		.find({})
+		.then((data) => {
+			res.json(data);
+		}).catch((err) => {
+			res.send(err);
+		});
 	}
+
+	static readNetworkStation(req, res) {
+		NetworkStation
+		.find({
+			id: req.params.id
+		}).then((data) => {
+			res.json(data);
+		}).catch((err) => {
+			res.send(err);
+		});
+	} 
 }
 
 module.exports = NetworkController;
